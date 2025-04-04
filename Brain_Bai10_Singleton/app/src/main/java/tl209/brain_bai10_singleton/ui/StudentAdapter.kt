c509397b663ee70b3d2381efc406ea30d4155eb0
ttdat <tandattranld@gmail.com>
2025-04-04 17:39:32 +0700

    Update V2

--- Brain_Bai10_Singleton/app/src/main/java/tl209/brain_bai10_singleton/ui/StudentAdapter.kt
+++ Brain_Bai10_Singleton/app/src/main/java/tl209/brain_bai10_singleton/ui/StudentAdapter.kt
@@ -1,4 +1,64 @@
 package tl209.brain_bai10_singleton.ui
 
-class StudentAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import tl209.brain_bai10_singleton.R
+import tl209.brain_bai10_singleton.data.model.Student
+import tl209.brain_bai10_singleton.utils.Utils
+
+class StudentAdapter(
+    private val listener: OnStudentClickListener
+): RecyclerView.Adapter<StudentAdapter.StudentViewHolder>() {
+    private val students = mutableListOf<Student>()
+
+    class StudentViewHolder(
+        itemView: View,
+        private val listener: OnStudentClickListener
+    ): RecyclerView.ViewHolder(itemView){
+        private val imageAvatar: ImageView = itemView.findViewById(R.id.image_avatar)
+        private val textId: TextView = itemView.findViewById(R.id.text_student_id)
+        private val textFullName: TextView = itemView.findViewById(R.id.text_full_name)
+        private val textGpa: TextView = itemView.findViewById(R.id.text_gpa)
+
+        fun bind(student: Student){
+            textId.text = student.id
+            textFullName.text = student.fullName.toString()
+            textGpa.text = student.gpa.toString()
+            imageAvatar.setImageResource(Utils.getAvatar(student.gender))
+            itemView.setOnClickListener{
+                listener.onClick(student)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val rootView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_student, parent, false)
+        return StudentViewHolder(rootView,listener)
+    }
+
+    override fun getItemCount(): Int {
+        return students.size
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        val student = students[position]
+        holder.bind(student)
+    }
+
+    fun updateStudents(studentList: List<Student>){
+        val oldSize = students.size
+        students.clear()
+        students.addAll(studentList)
+        notifyItemRangeRemoved(0, oldSize)
+        notifyItemRangeInserted(0, students.size)
+    }
+
+    interface OnStudentClickListener{
+        fun onClick(student: Student)
+    }
 }
--- Brain_Bai10_Singleton/app/src/main/java/tl209/brain_bai10_singleton/utils/Utils.kt
+++ Brain_Bai10_Singleton/app/src/main/java/tl209/brain_bai10_singleton/utils/Utils.kt
@@ -1,5 +1,30 @@
 package tl209.brain_bai10_singleton.utils
 
+import tl209.brain_bai10_singleton.R
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 object Utils {
     const val DATE_FORMAT = "dd/MM/yyyy"
+    private val dateFormatter = SimpleDateFormat(DATE_FORMAT, Locale.US)
+
+    fun getAvatar(gender: String): Int{
+        return when (gender.lowercase()){
+            "nam" -> R.drawable.ic_man
+            else -> R.drawable.ic_woman
+        }
+    }
+
+    fun stringToDate(value: String): Date {
+        return try {
+            dateFormatter.parse(value) ?: Date()
+        }catch (e: Exception){
+            Date()
+        }
+    }
+
+    fun dateToString(value: Date): String{
+        return dateFormatter.format(value)
+    }
 }
